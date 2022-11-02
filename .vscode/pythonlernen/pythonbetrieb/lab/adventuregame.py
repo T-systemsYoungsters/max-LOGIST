0d8fd9300c7311f7a34f73f25b0cf4f82108e661
Max Wolfram <maxwolfram@freenet.de>
2022-11-02 10:13:16 +0100

    kleine einfache Testgames und chapter 7 lab

--- .vscode/pythonlernen/pythonbetrieb/lab/adventuregame.py
+++ .vscode/pythonlernen/pythonbetrieb/lab/adventuregame.py
@@ -1,16 +1,47 @@
+done=False
 room_list=[]
-room=["veranda", None, 1, 2 ,None]
+room=["You are in the veranda. There is a passage to the east and south.", None, 1, 2 ,None]
 room_list.append(room)
-room=["bath", None, None, None, 0]
+room=["You are in the bath. There is a passage to the west.", None, None, None, 0]
 room_list.append(room)
-room=["livingroom", 0, 3, 5, 4]
+room=["You are in the livingroom. There is a passage to the north, east, south and west.", 0, 3, 5, 4]
 room_list.append(room)
-room=["bedroom", None,None,None, 2]
+room=["You are in the bedroom. There is a passage to the west.", None,None,None, 2]
 room_list.append(room)
-room=["balcony",None,2,None,None]
+room=["You are on the balcony. There is a passage to the east.",None,2,None,None]
 room_list.append(room)
-room=["diningroom", 2, 6,None,None]
+room=["You are in the diningroom. There is a passage to the north and east.", 2, 6,None,None]
 room_list.append(room)
-room=["kitchen",None,None,None,5]
+room=["You are in the kitchen. There is a passage to the west.",None,None,None,5]
 room_list.append(room)
 current_room=0
+while done == False:
+    print()
+    print(room_list[current_room][0])
+    choice=input("Do you want to go to the north(n), the east(e), the south(s) or the west(w)? ")
+    if choice=="n" :
+        next_room=room_list[current_room][1]
+        if next_room==None:
+            print("You can't go that way.")
+        else:
+            current_room=next_room
+    elif choice=="e" :
+        next_room=room_list[current_room][2]
+        if next_room==None:
+            print("You can't go that way.")
+        else:
+            current_room=next_room
+    elif choice=="s" :
+        next_room=room_list[current_room][3]
+        if next_room==None:
+            print("You can't go that way.")
+        else:
+            current_room=next_room
+    elif choice=="w" :
+        next_room=room_list[current_room][4]
+        if next_room==None:
+            print("You can't go that way.")
+        else:
+            current_room=next_room
+    else:
+        print("Wrong input.")
---
+++ .vscode/pythonlernen/pythonbetrieb/tests/flappybird.py
@@ -0,0 +1,29 @@
+import pygame
+import math
+import random
+import time
+pygame.init()
+# fenster erstellen
+screen = pygame.display.set_mode([600, 500])
+# Fenstertitel setzen
+pygame.display.set_caption("Flappybird")
+# steuert wie oft das Fenster geupdated wird
+zeit=pygame.time.Clock()
+# laufen lassen bis Fenster geschlossen wird
+running = True
+while running:
+# Test ob Fenster geschlossen wurde
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    for event in pygame.event.get():
+        if event.type == pygame.K_ESCAPE:
+            running=False
+# Hintergrund mit Farbe füllen
+    screen.fill((226, 0, 116))
+# updated das Fenster mit dem was man gezeichnet hat
+    pygame.display.flip()
+# auf 20 fps beschränkt
+zeit.tick(60)
+# schließen von pygame
+pygame.quit()
