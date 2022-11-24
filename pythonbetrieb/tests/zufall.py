a32997ababb028dc8198ec409d40c06817af6970
Max Wolfram <maxwolfram@freenet.de>
2022-11-24 15:55:31 +0100

    .

--- pythonbetrieb/tests/zufall.py
+++ pythonbetrieb/tests/zufall.py
@@ -1,8 +1,8 @@
 # zufällige Zahl wird in 10er Schritten bis 100 generiert
-import random
-zahl=random.randrange(0,100,10)
-print(zahl)
-print("----------------")
+# import random
+# zahl=random.randrange(0,100,10)
+# print(zahl)
+# print("----------------")
 # per choice kann man aus einer Liste zufällig einen wert auswählen
 # liste=["stein","schere","papier"]
 # wahl=random.choice(liste)
@@ -11,3 +11,14 @@ print("----------------")
 # zufällige zahl mit Kommarstellen, dessen bereich angepasst werden kann
 # neuezahl=random.random()*10+1
 # print(neuezahl)
+x = 5
+y = 10
+print("A")
+try:
+    print("B")
+    a = x / y
+    print("C")
+except:
+    print("D")
+print("E")
+print(a)
