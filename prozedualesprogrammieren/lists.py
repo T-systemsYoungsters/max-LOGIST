ce758acaf5ff96d8395f29f3dd9ed80b061b2083
Max Wolfram <maxwolfram@freenet.de>
2022-11-30 12:38:59 +0100

    .

--- prozedualesprogrammieren/lists.py
+++ prozedualesprogrammieren/lists.py
@@ -2,3 +2,19 @@
 # 1.a=2
 # 1.b=3,2,7,9
 # 1.c=3
+
+#Bubble
+import random
+list = []
+for i in range(10):
+    list.append(random.randrange(-100,100))
+print("unsorted")
+for i in range(len(list)-1):
+    for j in range(0, len(list) - i - 1):
+      if list[j] > list[j + 1]:
+        # temp = list[j]
+        # list[j] = list[j+1]
+        # list[j+1] = temp
+        list[j],list[j+1]=list[j+1],list[j]
+print("sorted")
+print(list)
