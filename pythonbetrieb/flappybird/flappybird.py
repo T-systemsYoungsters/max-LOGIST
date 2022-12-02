994294c384df8ed5cd010b3254482ac2cc83a605
Max Wolfram <maxwolfram@freenet.de>
2022-12-02 12:44:25 +0100

    .

--- pythonbetrieb/flappybird/flappybird.py
+++ pythonbetrieb/flappybird/flappybird.py
@@ -35,6 +35,28 @@ score_player=0
 status=-1
 done = False
 
+# files
+jump=pygame.mixer.Sound("jump.mp3")
+point=pygame.mixer.Sound("point.mp3") 
+die=pygame.mixer.Sound("die.mp3")
+backgroundmusic=pygame.mixer.music.load("song.mp3")
+backgroundmusic=pygame.mixer.music.play(-1,-10.0)
+bird_image=pygame.image.load("flappybird.png")
+bird_image.set_colorkey(WHITE)
+bird = bird_image.get_rect()
+pipeb_image=pygame.image.load("pipeb.png")
+pipeb_image.set_colorkey(WHITE)
+pipe1b= pipeb_image.get_rect()
+pipe2b= pipeb_image.get_rect()
+pipet_image=pygame.image.load("pipet.png")
+pipet_image.set_colorkey(WHITE)
+pipe1t= pipet_image.get_rect()
+pipe2t= pipet_image.get_rect()
+
+# screen
+screen = pygame.display.set_mode([width, hight])
+pygame.display.set_caption("My Game")
+
 # definitions
 def pressedenter():
     global status,birdy,score_player,pipex1,pipex2
@@ -70,7 +92,7 @@ def birdmovement():
         birdy=(hight-36.1)
 
 def pipemovement():
-    global pipex1,pipex2,b,c
+    global pipex1,pipex2,b,c,pipey1,pipey2
     pipex1-=(width*0.0003)
     pipex2-=(width*0.0003)
     if pipex1<-50:
@@ -79,29 +101,38 @@ def pipemovement():
     if pipex2<-50:
         pipex2=width
         c=0
-
-def drawpipes():
-    global pipe1,pipe2,pipe3,pipe4,b,c,pipey1,pipey2
     if b==0:
         pipey1=random.randrange(50,hight-200-50)
         b=1
     if c==0:
         pipey2=random.randrange(50,hight-200-50)
         c=1
-    pipe1=pygame.draw.rect(screen, (0, 200, 0), [pipex1, 0, 50, pipey1])
-    pipe2=pygame.draw.rect(screen, (0, 200, 0), [pipex1, pipey1+200, 50, hight-pipey1+200])
-    pipe3=pygame.draw.rect(screen, (0, 200, 0), [pipex2, 0, 50, pipey2])
-    pipe4=pygame.draw.rect(screen, (0, 200, 0), [pipex2, pipey2+200, 50, hight-pipey2+200])
+
+def blititems():
+    screen.blit(bird_image,( 100,birdy))
+    screen.blit(pipeb_image,( pipex1,pipey1+200))
+    screen.blit(pipeb_image,( pipex2,pipey2+200))
+    screen.blit(pipet_image,( pipex1,-1000+pipey1))
+    screen.blit(pipet_image,( pipex2,-1000+pipey2))
 
 def collisioncheck():
     global status,score_player
     bird.x =100
     bird.y =birdy
-    collide1 = bird.colliderect(pipe1)
-    collide2 = bird.colliderect(pipe2)
-    collide3 = bird.colliderect(pipe3)
-    collide4 = bird.colliderect(pipe4)
+    pipe1b.x =pipex1
+    pipe1b.y =pipey1+200
+    pipe2b.x =pipex2
+    pipe2b.y =pipey2+200
+    pipe1t.x =pipex1
+    pipe1t.y =-1000+pipey1
+    pipe2t.x =pipex2
+    pipe2t.y =-1000+pipey2
+    collide1 = bird.colliderect(pipe1b)
+    collide2 = bird.colliderect(pipe2b)
+    collide3 = bird.colliderect(pipe1t)
+    collide4 = bird.colliderect(pipe2t)
     if collide1==1 or collide2==1 or collide3==1 or collide4==1:
+        die.play()
         status=1
     else:
         if 50-(width*0.0003)<=pipex1<=50 or 50-(width*0.0003)<=pipex2<=50:
@@ -133,19 +164,9 @@ def updatehighscore():
     file.close()
     text3 = font.render("The Highscore is: " + highscore_list[-1], True, WHITE)   
 
-# files
-jump=pygame.mixer.Sound("jump.mp3")
-point=pygame.mixer.Sound("point.mp3") 
-bird_image=pygame.image.load("flappybird.png")
-bird_image.set_colorkey(WHITE)
-bird = bird_image.get_rect()
-
-# screen
-screen = pygame.display.set_mode([width, hight])
-pygame.display.set_caption("My Game")
-
 # main programm loop
 while not done:
+    
 # userinputs 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -169,11 +190,10 @@ while not done:
 # running game
     elif status==0:
         pipemovement()
-        drawpipes()
         birdmovement()
         collisioncheck()
         showscore(score_player) 
-        screen.blit(bird_image,( 100,birdy))
+        blititems()
 
 # game over
     elif status==1:
